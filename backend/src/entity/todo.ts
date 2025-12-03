8e8cb38512ad3b537585a6f8a6a041dd9d6cca2e
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2025-12-03 22:53:53 +0900

    Install Type ORM & create todo's model

---
+++ backend/src/entity/todo.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('todos')
+
+export class todos {
+    // Define columns and properties here
+    @PrimaryGeneratedColumn('uuid')
+    readonly id!: string;
+
+    // Define other columns here
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    public title!: string;
+
+    @Column({ type: 'text', nullable: false })
+    public content!: string;
+
+    @Column({ type: 'boolean', default: false })
+    public completed!: boolean;
+
+    @CreateDateColumn({ name: 'created_at' })
+    readonly createdAt!: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    readonly updatedAt!: Date;
+}
