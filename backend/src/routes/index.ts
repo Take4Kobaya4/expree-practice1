4f912094b6973232bd174977b3bd4c0fc325ee90
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2025-12-03 20:05:53 +0900

    first commit

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+// expressにルーティングを設定するためのRouterをインストール
+import { Router } from "express";
+
+
+const apiRouter = Router();
+// ルーティングを設定
+apiRouter.use();
+
+export default apiRouter;
---
+++ backend/src/routes/todo.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const todoRouter = Router();
+
+// Todoルート一覧（一覧、詳細、作成、更新、削除の順）
+// todoRouter.get('/', getAllTodoListHandler);
+// todoRouter.get('/:id', getTodoByIdHandler);
+// todoRouter.post('/', createTodoHandler);
+// todoRouter.put('/:id', updateTodoHandler);
+// todoRouter.delete('/:id', deleteTodoHandler);
+
+
+export default todoRouter;
